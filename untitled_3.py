a5d27957660718010633ef9d5243719944ab90bf
VictoriaLind <victoria@lindstar.fi>
2023-01-20 14:32:43 +0200

    still works 20/01/2023

--- untitled_3.py
+++ untitled_3.py
@@ -1,12 +1,5 @@
-#---NOTES---#
-# in qt, the side menu frames have a layout of:
-# LayoutLeftmargin: 15
-# LayoutTopmargin: 15
-# LayoutRightmargin: 0 # Because it has to connect to the edge of frame for the #TODO: side menu active effect
-# LayoutBottommargin: 15
 
-
-#----#FIXME: fix imports-----#
+# ----#FIXME: fix imports-----#
 import sys
 
 from PyQt5.QtWidgets import *
@@ -16,19 +9,18 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+import json
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         loadUi('untitled_3.ui', self)
 
         self.todoList = self.todoListWidget
-        #add task to todolist
-        self.todoList.addItem("test")
-        self.todoList.addItem("test2")
-
         self.todoAddtaskLE = self.todoAddTaskLineEdit
         self.todoAddtaskPB = self.todoAddTaskPushButton
-        
+
         self.inProgressList = self.inProgressListWidget
         self.inPAddtaskLE = self.inPAddTaskLineEdit
         self.inPAddtaskPB = self.inPAddTaskPushButton
@@ -40,6 +32,12 @@ class MainWindow(QMainWindow):
         self.todoAddtaskPB.clicked.connect(self.add_task_todo)
         self.inPAddtaskPB.clicked.connect(self.add_task_in_progress)
         self.doneAddtaskPB.clicked.connect(self.add_task_done)
+
+        self.load_tasks()
+
+    def closeEvent(self, event):
+        self.save_tasks()
+        event.accept()
 
     def add_task_todo(self):
         try:
@@ -56,7 +54,7 @@ class MainWindow(QMainWindow):
             self.inPAddtaskLE.clear()
         except Exception as e:
             print(e)
-    
+
     def add_task_done(self):
         try:
             task_text = self.doneAddtaskLE.text()
@@ -64,7 +62,54 @@ class MainWindow(QMainWindow):
             self.doneAddtaskLE.clear()
         except Exception as e:
             print(e)
-    
+
+    def save_tasks(self):
+        try:
+            tasks = {}
+            for i in range(self.todoList.count()):
+                item = self.todoList.item(i)
+                tasks[item.text()] = "todo"
+
+            for i in range(self.inProgressList.count()):
+                item = self.inProgressList.item(i)
+                tasks[item.text()] = "in progress"
+
+            for i in range(self.doneList.count()):
+                item = self.doneList.item(i)
+                tasks[item.text()] = "done"
+
+            with open("tasks.json", "w") as f:
+                json.dump(tasks, f)
+        except FileNotFoundError:
+            print("tasks.json not found. Creating a new one.")
+            with open("tasks.json", "w") as f:
+                json.dump({}, f)
+        except json.decoder.JSONDecodeError:
+            print("tasks.json is corrupted. Creating a new one.")
+            with open("tasks.json", "w") as f:
+                json.dump({}, f)
+
+    def load_tasks(self):
+        try:
+            with open("tasks.json", "r") as f:
+                tasks = json.load(f)
+            for task, status in tasks.items():
+                if status == "todo":
+                    self.todoList.addItem(task)
+                elif status == "in progress":
+                    self.inProgressList.addItem(task)
+                elif status == "done":
+                    self.doneList.addItem(task)
+        except FileNotFoundError:
+            print("tasks.json not found. Creating a new one.")
+            with open("tasks.json", "w") as f:
+                json.dump({}, f)
+        except json.decoder.JSONDecodeError:
+            print("tasks.json is corrupted. Creating a new one.")
+            with open("tasks.json", "w") as f:
+                json.dump({}, f)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
